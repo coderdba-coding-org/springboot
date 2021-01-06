2d05681f34d85faa0eff23947ebf2665a2b27244
coderdba <gowrishdba@yahoo.com>
2021-01-06 15:38:20 +0530

    more changes

--- various/metrics01/src/main/java/com/gm/metrics/AppMetricRegistry.java
+++ various/metrics01/src/main/java/com/gm/metrics/AppMetricRegistry.java
@@ -50,6 +50,7 @@ public class AppMetricRegistry {
 	    
 	}
     
+    /*
     @Configuration
     public class ServiceConfig {
 
@@ -59,5 +60,6 @@ public class AppMetricRegistry {
         }
 
     }
+    */
 
 }
---
+++ various/metrics01/src/main/java/com/gm/metrics/Config.java
@@ -0,0 +1,20 @@
+package com.gm.metrics;
+
+import com.codahale.metrics.MetricRegistry;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Config {
+
+    @Configuration
+    public class ServiceConfig {
+
+        @Bean
+        public MetricRegistry getMetricsRegistry(){
+            return new MetricRegistry();
+        }
+
+    }
+}
