0afdf8212ab2b77751c4d882066d81ec56a4c6e6
coderdba <gowrishdba@yahoo.com>
2021-01-21 18:28:57 +0530

    more changes

---
+++ various/postgres01/src/main/java/com/gm/postgres/services/BookService.java
@@ -0,0 +1,20 @@
+package com.gm.postgres.services;
+
+import com.gm.postgres.models.Book;
+import com.gm.postgres.repositories.BookRepository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    public List<Book> list() {
+        return bookRepository.findAll();
+    }
+}
