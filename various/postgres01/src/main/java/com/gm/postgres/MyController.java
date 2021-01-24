a08da8f46c9fc8b3805e3fca0174f520ab90f686
coderdba <gowrishdba@yahoo.com>
2021-01-24 20:03:59 +0530

    more changes

---
+++ various/postgres01/src/main/java/com/gm/postgres/MyController.java
@@ -0,0 +1,33 @@
+package com.gm.postgres;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// Reference: https://zetcode.com/springboot/postgresql/
+
+import com.gm.postgres.models.Book;
+import com.gm.postgres.services.BookService;
+import com.gm.postgres.repositories.BookRepository;
+
+@RestController
+public class MyController {
+
+	@Autowired
+	private BookService bookService;
+	
+    @GetMapping("/showBooks")
+    public List showBooks() {
+
+        //var books = (List<Book>) bookService.list();
+
+        //model.addAttribute("cities", cities);
+
+        //return "showCities";
+    	
+    	return bookService.list();
+    }
+	
+}
